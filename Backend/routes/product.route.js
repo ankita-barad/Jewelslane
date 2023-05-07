8ab9fdb58f33de40f228af49625ac29aac8d95a2
ankita-barad <ankitabarad2111@gmail.com>
2023-05-07 20:06:25 +0530

    frontend productDetails page

--- Backend/routes/product.route.js
+++ Backend/routes/product.route.js
@@ -22,4 +22,14 @@ productRoute.get("/", async (req, res) => {
   }
 });
 
+productRoute.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    let product = await ProductModel.findById({ _id: id });
+    res.status(200).send(JSON.stringify(product));
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 module.exports = { productRoute };
